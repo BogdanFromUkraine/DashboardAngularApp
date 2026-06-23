340a940feb69560e739d0ceb9a333a4952f1a4cb
Bogdan_Boyk0 <104592168+BogdanFromUkraine@users.noreply.github.com>
2026-06-23 13:08:56 +0300

    feat: configure client routing and http provider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { TaskBoardComponent } from './components/task-board/task-board.component';
+import { BookmarkLinksComponent } from './components/bookmark-links/bookmark-links.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'profile', pathMatch: 'full' },
+  {path: 'profile', component: GithubProfileComponent},
+  {path: 'task', component: TaskBoardComponent},
+  {path: 'links', component: BookmarkLinksComponent},
+
+  {path: '**', redirectTo: 'profile'},
+];
